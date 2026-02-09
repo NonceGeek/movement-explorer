d0d10860c3b16dfaae2d51731521340fd88ed619
Fynn <0xfynnix@gmail.com>
2026-02-09 18:28:52 +0800

    Adjust background element width from `w-[400px]` to `w-100` and reformat child div.

--- src/components/layout/LayoutBackground.tsx
+++ src/components/layout/LayoutBackground.tsx
@@ -35,14 +35,12 @@ export function LayoutBackground({ children }: LayoutBackgroundProps) {
         aria-hidden="true"
       />
       <div
-        className="fixed top-8 left-1/2 -translate-x-1/2 w-[400px] h-[200px] bg-byzantine-blue-500/10 blur-[80px] rounded-full pointer-events-none z-10"
+        className="fixed top-8 left-1/2 -translate-x-1/2 w-100 h-[200px] bg-byzantine-blue-500/10 blur-[80px] rounded-full pointer-events-none z-10"
         aria-hidden="true"
       />
 
       {/* Layer 4: Content */}
-      <div className="relative z-20 flex-1 flex flex-col">
-        {children}
-      </div>
+      <div className="relative z-20 flex-1 flex flex-col">{children}</div>
     </div>
   );
 }
